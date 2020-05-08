4a383ced1d5c207ae7d1fd5b4937bc8e4ea23350
Griko <namba19960409@gmail.com>
2020-05-08 17:24:12 +0900

    変更してみた

---
+++ Asahi/src/main/java/asahi/TestPrac.java
@@ -0,0 +1,14 @@
+package asahi;
+
+public class TestPrac {
+	public static void main(String[] args) {
+		int[] num = new int[3];
+		num[0] = 3;
+		num[1] = 2;
+		num[2] = 0;
+		for(int i = 0; i<3;i++) {
+			System.out.println(num[i]);
+		}
+	}
+
+}
