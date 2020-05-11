d902816f4a1411df52057166ee88af83d4289601
Griko <namba19960409@gmail.com>
2020-05-11 11:28:09 +0900

    プルリクエストに応えてみた

--- Asahi/src/main/java/asahi/Dog.java
+++ Asahi/src/main/java/asahi/Dog.java
@@ -1,9 +1,15 @@
 package asahi;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Dog {
 	
 	private String name;
 
+	@Setter @Getter
+	private int age;
+	
 	//名前のset
 	public void SetName(String name) {
 		this.name = name;
